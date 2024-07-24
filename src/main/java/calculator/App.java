32ca7953a951e9c81ba08617ef051cf1d044209f
MinJun Song <j34679@naver.com>
2024-07-24 17:14:45 +0900

    array -> List changed

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,17 +1,19 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num1 = 0, num2 = 0;
-        int[] result = new int[10];
+        ArrayList<Integer> resultList = new ArrayList<Integer>();
         int resultIndex = 0;
         int tempResult = 0;
         int count = 0;
         String operator = "";
         String input = "";
+
         while (true) {
             while (true) {
                 System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -25,33 +27,32 @@ public class App {
                 } else
                     break;
             }
-            result[9] = tempResult;
+            System.out.print("사칙연산 기호를 입력하세요: ");
             do {
-                System.out.print("사칙연산 기호를 입력하세요: ");
                 // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
                 operator = sc.next();
                 switch (operator) {
                     case "+":
                         tempResult = num1 + num2;
-                        result[resultIndex] = tempResult;
+                        resultList.add(tempResult);
                         System.out.println("결과: " + tempResult);
                         break;
 
                     case "-":
                         tempResult = num1 - num2;
-                        result[resultIndex] = tempResult;
+                        resultList.add(tempResult);
                         System.out.println("결과: " + tempResult);
                         break;
 
                     case "*":
                         tempResult = num1 * num2;
-                        result[resultIndex] = tempResult;
+                        resultList.add(tempResult);
                         System.out.println("결과: " + tempResult);
                         break;
 
                     case "/":
                         tempResult = num1 / num2;
-                        result[resultIndex] = tempResult;
+                        resultList.add(tempResult);
                         System.out.println("결과: " + tempResult);
                         break;
 
@@ -63,14 +64,15 @@ public class App {
                 }
             }
             while (!(operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")));
-            resultIndex++;
-            count++;
-            if (count >= 10) {
-                resultIndex = 9;
-                for (int i = 0; i < 9; i++) {
-                    result[i] = result[i + 1];
-                }
+
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력시 삭제, 아무 키나 입력시 삭제 X");
+            input = sc.next();
+            if(input.equals("remove"))
+            {
+                resultList.remove(0);
             }
+
+
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료), 1 입력시 재시작");
             while (true) {
